51b36089f09d7af09d4bb419a2b3024b9a1eb7f5
jefferen <jeff0167@edu.zealand.dk>
2023-04-06 12:03:18 +0200

    We now have Docs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,20 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDHihjHGIy7ft2ZLy5sPxLWuFoUSO8f91o",
+
+  authDomain: "studentapp-289c8.firebaseapp.com",
+
+  databaseURL: "https://studentapp-289c8-default-rtdb.europe-west1.firebasedatabase.app",
+
+  projectId: "studentapp-289c8",
+
+  storageBucket: "studentapp-289c8.appspot.com",
+
+  messagingSenderId: "168959365364",
+
+  appId: "1:168959365364:web:4f3ac4c945d1c8bb3c0ea1",
+
+  measurementId: "G-C4PML4WLYN"
+  }
 };
